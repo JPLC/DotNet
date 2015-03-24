adf66ed8bed426f657c31cbb070aa9cdf4cfb519
JPLC <joao_caetano10@hotmail.com>
2015-03-24 23:45:10 +0000

    Added Repositories,TrelloDbInit and UnitTest project

---
+++ Web API Examples/TrelloModel/TrelloContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrelloModel
+{
+    public class TrelloContext : DbContext
+    {
+        public TrelloContext() : base("TrelloContext")
+        {
+        }
+
+        public DbSet<Board> Boards { get; set; }
+
+        public DbSet<List> Lists { get; set; }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
