965bacbdbc2b957628380e81d89afad403ce3e1f
JPLC <joao_caetano10@hotmail.com>
2015-03-25 12:21:38 +0000

    irepository added
    irepository added

---
+++ Web API Examples/TrelloModel/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TrelloModel.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+
+        T GetSingle(int fooId);
+
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Edit(T entity);
+    }
+}
