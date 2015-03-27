ca55aa52801c2b09791e3d88352e35d733865d50
JPLC <joao_caetano10@hotmail.com>
2015-03-27 18:07:01 +0000

    Added Web API Trello Project and Removed the old one

---
+++ Web API Examples/TrelloWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TrelloWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Web API Examples/TrelloWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrelloWebAPI.WebApiApplication" Language="C#" %>
