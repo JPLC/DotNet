0514da9460d4b69fc7233572cdff4260ca1b08c9
JPLC <joao_caetano10@hotmail.com>
2015-04-01 22:27:39 +0100

    Added Api Controllers Tests

---
+++ Web API Examples/TrelloWebAPI.Tests/Controllers/BoardControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrelloWebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class BoardControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Web API Examples/TrelloWebAPI.Tests/Controllers/ListControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrelloWebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class ListControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
