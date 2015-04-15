62a7d2abddf53c9b2f02212aa4835c6b814979cb
JPLC <joao_caetano10@hotmail.com>
2015-04-16 00:46:56 +0100

    added missing classes

---
+++ Web API Examples/TrelloMVC/ViewModelsConverters/BoardViewModelConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrelloModel;
+using TrelloMVC.ViewModels.BoardViewModels;
+
+namespace TrelloMVC.ViewModelsConverters
+{
+    /*TODO FInish view model converters*/
+    public static class BoardViewModelConverter
+    {
+
+        public static BoardsViewModel ModelToViewModel(Board board)
+        {
+            return new BoardsViewModel{ Id = board.BoardId, Name = board.Name, Discription = board.Discription};
+        }
+
+        public static IEnumerable<BoardsViewModel> ModelsToViewModels(IEnumerable<Board> boards)
+        {
+            return boards.Select(board => new BoardsViewModel {Id = board.BoardId, Name = board.Name, Discription = board.Discription}).ToList();
+        }
+
+       /* public static Board ModelToViewModel(BoardsViewModel boardsViewModel)
+        {
+        }*/
+    }
+}
