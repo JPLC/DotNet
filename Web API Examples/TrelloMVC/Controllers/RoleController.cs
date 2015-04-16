754316d296f186ff521713db006a5771b2041375
JPLC <joao_caetano10@hotmail.com>
2015-04-17 00:23:34 +0100

    t

---
+++ Web API Examples/TrelloMVC/Controllers/RoleController.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using TrelloMVC.Models;
+
+namespace TrelloMVC.Controllers
+{
+    [RoutePrefix("Roles")]
+    /*TODO Trocar ViewBags por ViewModels*/
+    public class RoleController : Controller
+    {
+        readonly ApplicationDbContext _context = new ApplicationDbContext();
+        #region Variables
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        #endregion
+
+        #region Properties
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public RoleController()
+        {
+
+        }
+
+        public RoleController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        #endregion
+
+        #region Action Methods
+        // GET: /Roles/All
+        [Route("All")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var roles = _context.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: /Roles/Create
+        [HttpGet]
+        [Route("Create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Roles/Create
+        [HttpPost]
+        [Route("Create")]
+        /*TODO Criar view model em vez do form collection*/
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                _context.Roles.Add(new IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                _context.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: /Roles/Edit/roleName
+        [HttpGet]
+        [Route("Edit/{roleName}")]
+        public ActionResult Edit(string roleName)
+        {
+            var thisRole = _context.Roles.FirstOrDefault(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase));
+            if (thisRole == null)
+                return HttpNotFound();
+            return View(thisRole);
+        }
+
+        // POST: /Roles/Edit/roleName
+        [HttpPost]
+        [Route("Edit/{roleName}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(IdentityRole role)
+        {
+            try
+            {
+                _context.Entry(role).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: /Roles/Delete/rolename
+        [HttpGet]
+        [Route("Delete/{roleName}")]
+        public ActionResult Delete(string roleName)
+        {
+            var thisRole = _context.Roles.FirstOrDefault(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase));
+            _context.Roles.Remove(thisRole);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("ManageUserRoles")]
+        public ActionResult ManageUserRoles()
+        {
+            var list = _context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("RoleAddToUser")]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string userName, string roleName)
+        {
+            ApplicationUser user = UserManager.Users.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.CurrentCultureIgnoreCase));
+            
+            if (user != null)
+                UserManager.AddToRole(user.Id, roleName);
+
+            ViewBag.ResultMessage = "Role created successfully !";
+
+            // prepopulat roles for the view dropdown
+            var list = _context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [Route("GetRoles")]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string userName)
+        {
+            if (!string.IsNullOrWhiteSpace(userName))
+            {
+                ApplicationUser user = _context.Users.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.CurrentCultureIgnoreCase));
+                
+
+                if (user != null)
+                    ViewBag.RolesForThisUser = UserManager.GetRoles(user.Id);
+
+                // prepopulat roles for the view dropdown
+                var list = _context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = list;
+            }
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [Route("DeleteRoleForUser")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleForUser(string userName, string roleName)
+        {
+            
+            var user = _context.Users.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.CurrentCultureIgnoreCase));
+
+            if (user != null && UserManager.IsInRole(user.Id, roleName))
+            {
+                UserManager.RemoveFromRole(user.Id, roleName);
+                ViewBag.ResultMessage = "Role removed from this user successfully !";
+            }
+            else
+            {
+                ViewBag.ResultMessage = "This user doesn't belong to selected role.";
+            }
+            // prepopulat roles for the view dropdown
+            var list = _context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("ManageUserRoles");
+        }
+        #endregion
+    }
+}
