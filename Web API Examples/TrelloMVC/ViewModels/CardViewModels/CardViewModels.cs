e6de81b535176d98fd5129a9721aaa44c0ccb6d2
JPLC <joao_caetano10@hotmail.com>
2015-04-24 00:40:01 +0100

    t

---
+++ Web API Examples/TrelloMVC/ViewModels/CardViewModels/CardViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrelloMVC.ViewModels.CardViewModels
+{
+    public class CardViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Cix { get; set; }
+
+        public string Discription { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public string ListName { get; set; }
+    }
+}
