e3c95a31d8a880048d3f5cfb1e5c27a78597007e
JPLC <joao_caetano10@hotmail.com>
2015-04-23 13:03:52 +0100

    t

---
+++ Web API Examples/TrelloMVC/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+using System.Data.Entity.Migrations;
+using Microsoft.AspNet.Identity;
+using TrelloMVC.Models;
+
+namespace TrelloMVC.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "TrelloMVC.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            var passwordHash = new PasswordHasher();
+              context.Users.AddOrUpdate(
+                  new ApplicationUser { UserName = "JPLC",  PasswordHash = passwordHash.HashPassword("jplc1989!") },
+                  new ApplicationUser { UserName = "Caetano",    PasswordHash = passwordHash.HashPassword("jplc1989!") },
+                  new ApplicationUser { UserName = "Pedro",  PasswordHash = passwordHash.HashPassword("jplc1989!") },
+                  new ApplicationUser { UserName = "Maria",  PasswordHash = passwordHash.HashPassword("jplc1989!") }
+                );
+            //
+        }
+    }
+}
