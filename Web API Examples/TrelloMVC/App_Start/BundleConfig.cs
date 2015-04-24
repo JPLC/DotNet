b9fefe3b7d06a3ec5d3c071f2458f6d33f1ba066
JPLC <joao_caetano10@hotmail.com>
2015-04-24 17:26:07 +0100

    added social buttons

--- Web API Examples/TrelloMVC/App_Start/BundleConfig.cs
+++ Web API Examples/TrelloMVC/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace TrelloMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/zocial.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/unobtrusive").Include(
                 "~/Scripts/jquery.unobtrusive*"));
